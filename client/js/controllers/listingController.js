accf36630e4561483f6437883943dd5116e24332
hbrian <bho123@ufl.edu>
2019-02-11 21:11:41 -0500

    fixed controller

--- client/js/controllers/listingController.js
+++ client/js/controllers/listingController.js
@@ -10,21 +10,42 @@ angular.module('listings').controller('ListingsController', ['$scope', 'Listings
     $scope.detailedInfo = undefined;
 
     $scope.addListing = function() {
+      var newListEntry = {
+        name: $scope.newListing.name,
+        code: $scope.newListing.code,
+        address: $scope.newListing.address
+      };
+
       $scope.listings.push($scope.newListing);
-	  /**TODO 
-	  *Save the article using the Listings factory. If the object is successfully 
+      $scope.newListing = {};
+
+      Listings.create(newListEntry).then(function(response) {
+        $scope.newListing.name = '';
+        $scope.newListing.code = '';
+        $scope.newListing.address = '';
+      }, function(err){
+        $scope.err = 'Listing not saved:' + err;
+      });
+    /*
+     *Save the article using the Listings factory. If the object is successfully 
 	  saved redirect back to the list page. Otherwise, display the error
 	 */
     };
 
     $scope.deleteListing = function(index) {
-     $scope.listings.splice(index, 1);
+      var entry = $scope.listings[index];
+      var entryID = entry._id;
 
-	   /**TODO
+      Listings.delete(entryID).then(function(response) {
+        console.log('unable to delete')},
+        function(err) {console.log('error')});
+        $scope.listings.splice(index,1);
+      };
+
+	   /*
         Delete the article using the Listings factory. If the removal is successful, 
 		navigate back to 'listing.list'. Otherwise, display the error. 
        */
-    };
 
     $scope.showDetails = function(index) {
       $scope.detailedInfo = $scope.listings[index];
